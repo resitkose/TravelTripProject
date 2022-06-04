3a1c1b939438a94a5096c99c232ef9050bb0eb2f
RESİT <resit.kose@hotmail.com>
2022-06-04 18:25:37 +0300

    Add project files.

---
+++ TravelTripProject/Controllers/AdminController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        // GET: Admin
+        [Authorize]
+        public ActionResult Index()
+        {
+            var deger = c.Blogs.ToList();
+            return View(deger);
+        }
+        [HttpGet,Authorize]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(Blog p)
+        {
+            c.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //public ActionResult BlogSil(int id)
+        //{
+        //    var b = c.Blogs.Find(id);       2.silme Alternatifi
+        //    c.Blogs.Remove(b);
+        //    c.SaveChanges();
+        //    return RedirectToAction("Index");
+
+        //}
+        [Authorize]
+        public ActionResult BlogGetir(int id)
+        {
+            var b = c.Blogs.Find(id);
+            return View("BlogGetir", b);
+        }
+        public ActionResult BlogGuncelle(Blog p)
+        {
+            c.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(Yorumlar p)
+        {
+            c.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        [Authorize]
+        public ActionResult YorumGetir(int id)
+        {
+            var yrm = c.Yorumlars.Find(id);
+            return View("YorumGetir", yrm);
+        }
+        //public ActionResult YorumGuncelle(Yorumlar y)
+        //{
+        //    c.Entry(y).State = System.Data.Entity.EntityState.Modified;
+        //    c.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var deger = c.Blogs.ToList();
+            return View(deger);
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var deger = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(10).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
+            return PartialView(deger);
+        }
+    }
+}
